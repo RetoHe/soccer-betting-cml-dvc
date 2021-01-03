81381eac10def63bf26aec169807c0790547b6ef
RetoH <1910837262@stud.fh-kufstein.ac.at>
2021-01-03 16:49:33 +0100

    change workflow file

---
+++ prepare_data.py
@@ -0,0 +1,9 @@
+import pandas as import pd
+import numpy as np
+
+# Import Data
+data = pd.read_csv("data/soccerdata.csv")
+# Choose necessary colums
+data = data[["HomeTeam", "AwayTeam", "FTHG", "FTAG", "FTR", "HTHG", "HTAG", "HTR","HS","AS","HST","AST","HF","AF","HC","AC","HY","AY","HR","AR","B365H","B365D","B365A"]]
+# Export cleaned Dataframe to csv
+data.to_csv(r'data/soccerdata_cleaned.csv', index = False)
