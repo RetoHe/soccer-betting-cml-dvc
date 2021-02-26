85564a09721bc457b24f4c61ff24d31284d53f09
RetoH <1910837262@stud.fh-kufstein.ac.at>
2021-02-26 17:16:01 +0100

    Add graphic

--- historical_analysis.py
+++ historical_analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import os
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 directory = "data/historicaldata"
 files = os.listdir(directory)
@@ -11,6 +13,7 @@ for file in files:
     data = pd.read_csv(str(directory+"/"+file))
     score_gap_list = []
     score_gap = 0
+    cum_score_gap_list = []
     for i in range(len(data)):
         #if data["B365H"][i] < data["B365A"][i] and data["B365H"][i] < data["B365D"][i] and data["FTR"][i] == "H":
             #score_gap += ((data["B365H"][i]*1)-1)
@@ -18,12 +21,14 @@ for file in files:
         if data["B365A"][i] < data["B365H"][i] and data["B365A"][i] < data["B365D"][i] and data["FTR"][i] == "A":
             score_gap += ((data["B365A"][i]*1)-1)
             score_gap_list.append(((data["B365A"][i]*1)-1))
+            cum_score_gap_list.append(score_gap)
         #elif data["B365D"][i] < data["B365H"][i] and data["B365D"][i] < data["B365A"][i] and data["FTR"][i] == "D":
             #score_gap += ((data["B365D"][i]*1)-1)
             #score_gap_list.append(((data["B365D"][i]*1)-1))
         elif data["B365A"][i] < data["B365H"][i] and data["B365A"][i] < data["B365D"][i] and data["FTR"][i] != "A":
             score_gap += -1
             score_gap_list.append(-1)
+            cum_score_gap_list.append(score_gap)
         #elif data["B365H"][i] < data["B365A"][i] and data["B365H"][i] < data["B365D"][i] and data["FTR"][i] != "H":
             #score_gap += -1
             #score_gap_list.append(-1)
@@ -32,8 +37,14 @@ for file in files:
             #score_gap_list.append(-1)
         else:
             score_gap_list.append(0)
+            cum_score_gap_list.append(score_gap)
     season.append(file)
     scores.append(score_gap)
+    results_df[file] = cum_score_gap_list
+
+plt.figure()
+sns.lineplot(data=results_df)
+plt.savefig("output.png")
 
 with open("historical_score.txt", "w") as f:
     print("Historical Analysis:", file=f)
