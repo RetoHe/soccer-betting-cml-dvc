f3070c1940a7f9a3a820ae5478bcc8c5db909659
RetoH <1910837262@stud.fh-kufstein.ac.at>
2021-02-26 10:27:51 +0100

    Add new technlogy

--- historical_analysis.py
+++ historical_analysis.py
@@ -16,23 +16,23 @@ for file in files:
         if gap >= 5 and data["FTR"][i] == "H":
             score_gap += ((data["B365H"][i]*5)-5)
             score_gap_list.append(((data["B365H"][i]*5)-5))
-        elif gap <= 5 and gap >= 3 and data["FTR"][i] == "H":
-            score_gap += ((data["B365H"][i]*3)-3)
-            score_gap_list.append(((data["B365H"][i]*3)-3))
+       # elif gap <= 5 and gap >= 3 and data["FTR"][i] == "H":
+            # += ((data["B365H"][i]*3)-3)
+            #score_gap_list.append(((data["B365H"][i]*3)-3))
         elif gap <= -5 and data["FTR"][i] == "A":
             score_gap += ((data["B365A"][i]*5)-5)
             score_gap_list.append(((data["B365A"][i]*5)-5))
-        elif gap >= -5 and gap <= -3 and data["FTR"][i] == "A":
-            score_gap += ((data["B365A"][i]*3)-3)
-            score_gap_list.append(((data["B365A"][i]*3)-3))
+        #elif gap >= -5 and gap <= -3 and data["FTR"][i] == "A":
+            #score_gap += ((data["B365A"][i]*3)-3)
+            #score_gap_list.append(((data["B365A"][i]*3)-3))
         elif gap >= 5 and data["FTR"][i] != "H":
             score_gap += -5
-        elif gap <= 5 and gap >= 3 and data["FTR"][i] != "H":
-            score_gap += -3
+        #elif gap <= 5 and gap >= 3 and data["FTR"][i] != "H":
+            #score_gap += -3
         elif gap <= -5 and data["FTR"][i] != "A":
             score_gap += -5
-        elif gap >= -5 and gap <= -3 and data["FTR"][i] != "A":
-            score_gap += -3
+        #elif gap >= -5 and gap <= -3 and data["FTR"][i] != "A":
+            #score_gap += -3
         else:
             score_gap_list.append(0)
     season.append(file)
