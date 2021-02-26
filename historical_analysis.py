cd257343f80114259a9023f16bd7f8443cae5c34
RetoH <1910837262@stud.fh-kufstein.ac.at>
2021-02-26 16:33:58 +0100

    Home Win Strategy

--- historical_analysis.py
+++ historical_analysis.py
@@ -15,21 +15,21 @@ for file in files:
         if data["B365H"][i] < data["B365A"][i] and data["B365H"][i] < data["B365D"][i] and data["FTR"][i] == "H":
             score_gap += ((data["B365H"][i]*1)-1)
             score_gap_list.append(((data["B365H"][i]*1)-1))
-        elif data["B365A"][i] < data["B365H"][i] and data["B365A"][i] < data["B365D"][i] and data["FTR"][i] == "A":
-            score_gap += ((data["B365A"][i]*1)-1)
-            score_gap_list.append(((data["B365A"][i]*1)-1))
-        elif data["B365D"][i] < data["B365H"][i] and data["B365D"][i] < data["B365A"][i] and data["FTR"][i] == "D":
-            score_gap += ((data["B365D"][i]*1)-1)
-            score_gap_list.append(((data["B365D"][i]*1)-1))
-        elif data["B365A"][i] < data["B365H"][i] and data["B365A"][i] < data["B365D"][i] and data["FTR"][i] != "A":
-            score_gap += -1
-            score_gap_list.append(-1)
+        #elif data["B365A"][i] < data["B365H"][i] and data["B365A"][i] < data["B365D"][i] and data["FTR"][i] == "A":
+            #score_gap += ((data["B365A"][i]*1)-1)
+            #score_gap_list.append(((data["B365A"][i]*1)-1))
+        #elif data["B365D"][i] < data["B365H"][i] and data["B365D"][i] < data["B365A"][i] and data["FTR"][i] == "D":
+            #score_gap += ((data["B365D"][i]*1)-1)
+            #score_gap_list.append(((data["B365D"][i]*1)-1))
+        #elif data["B365A"][i] < data["B365H"][i] and data["B365A"][i] < data["B365D"][i] and data["FTR"][i] != "A":
+            #score_gap += -1
+            #score_gap_list.append(-1)
         elif data["B365H"][i] < data["B365A"][i] and data["B365H"][i] < data["B365D"][i] and data["FTR"][i] != "H":
             score_gap += -1
             score_gap_list.append(-1)
-        elif data["B365D"][i] < data["B365H"][i] and data["B365D"][i] < data["B365A"][i] and data["FTR"][i] != "D":
-            score_gap += -1
-            score_gap_list.append(-1)
+        #elif data["B365D"][i] < data["B365H"][i] and data["B365D"][i] < data["B365A"][i] and data["FTR"][i] != "D":
+            #score_gap += -1
+            #score_gap_list.append(-1)
         else:
             score_gap_list.append(0)
     season.append(file)
